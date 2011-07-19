e6ed09e07a5240033e3266a6fbc0b4ab817dc688
Eugene Awot <easyable01@gmail.com>
2011-07-19 10:58:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eaweb::Application.config.secret_token = '5a39096032b98a8e927406185b588c61722a588e6905a8b5792c12e2794f6d50cafa6380efc548d3ab021ef1d02b5c29cc656980455cc2ce7c22eb86f2acde73'
